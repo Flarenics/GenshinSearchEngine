e5ccaee8f5cb9a5b38d6d0c7e2b9406857c32512
Flarenics <flareonloverz@gmail.com>
2025-03-13 04:21:18 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,106 @@
+async function fetchInfo(){
+
+    const userInput = document.getElementById("searchPoints").value.toLocaleLowerCase();
+    console.log(userInput);
+
+    if (userInput == "character"){
+        try{
+            const characterName = document.getElementById("characterName").value.toLocaleLowerCase();
+            const response = await fetch(`https://genshin.jmp.blue/characters/${characterName}`)
+    
+            if(!response.ok){
+                throw new Error("Could not fetch again womp");
+            }
+    
+            const data = await response.json();
+            const characterSplash = (`https://genshin.jmp.blue/characters/${characterName}/gacha-splash`);
+            const characterCard = (`https://genshin.jmp.blue/characters/${characterName}/gacha-card`);
+            const characterIcon = (`https://genshin.jmp.blue/characters/${characterName}/icon-big`);
+    
+            const imgElementSplash = document.getElementById("characterSplash");
+            const imgElementCard = document.getElementById("characterCard");
+            const imgElementIcon = document.getElementById("characterIcon");
+            const charName = document.getElementById("charName");
+            const charTitle = document.getElementById("charTitle");
+            const charDesc = document.getElementById("charDesc");
+    
+            imgElementSplash.src = characterSplash;
+            imgElementSplash.style.display = "block";
+            imgElementCard.src = characterCard;
+            imgElementCard.style.display = "block";
+            imgElementIcon.src = characterIcon;
+            imgElementIcon.style.display = "block";
+            charName.textContent = data.name
+            charTitle.textContent = data.title
+            charDesc.textContent = data.description
+            
+            
+            console.log(data);
+        }
+        catch(error){
+            console.error(error);
+        }
+    } else if (userInput == "nation"){
+        try{
+            const characterName = document.getElementById("characterName").value.toLocaleLowerCase();
+            const response = await fetch(`https://genshin.jmp.blue/nations/${characterName}`)
+    
+            if(!response.ok){
+                throw new Error("Could not fetch again womp");
+            }
+    
+            const data = await response.json();
+            const nationIcon = (`https://genshin.jmp.blue/nations/${characterName}/icon`);
+    
+            const nationName = document.getElementById("nationName");
+            const nationElement = document.getElementById("nationElement");
+            const nationLeader = document.getElementById("nationLeader");
+            const imgElementIcon = document.getElementById("nationIcon");
+    
+            imgElementIcon.src = nationIcon;
+            imgElementIcon.style.display = "block";
+            nationName.textContent = data.name
+            nationElement.textContent = data.element
+            nationLeader.textContent = data.archon + ", " + data.controllingEntity
+            
+            console.log(data);
+        }
+        catch(error){
+            console.error(error);
+        }
+    } else if (userInput == "artifact"){
+        try{
+            const characterName = document.getElementById("characterName").value.toLocaleLowerCase();
+            const response = await fetch(`https://genshin.jmp.blue/artifacts/${characterName}`)
+    
+            if(!response.ok){
+                throw new Error("Could not fetch again womp");
+            }
+    
+            const data = await response.json();
+            const circletImg = (`https://genshin.jmp.blue/artifacts/${characterName}/circlet-of-logos`);
+            const flowerImg = (`https://genshin.jmp.blue/artifacts/${characterName}/flower-of-life`);
+            const gobletImg = (`https://genshin.jmp.blue/artifacts/${characterName}/goblet-of-eonothem`);
+            const plumeImg = (`https://genshin.jmp.blue/artifacts/${characterName}/plume-of-death`);
+            const sandsImg = (`https://genshin.jmp.blue/artifacts/${characterName}/sands-of-eon`);
+
+            const charName = document.getElementById("charName");
+            const charTitle = document.getElementById("charTitle");
+            const charDesc = document.getElementById("charDesc");
+            const imgElementIcon = document.getElementById("characterIcon");
+    
+            imgElementIcon.src = nationIcon;
+            imgElementIcon.style.display = "block";
+            charName.textContent = data.name
+            charTitle.textContent = data.element
+            charDesc.textContent = data.archon + ", " + data.controllingEntity
+            
+            console.log(data);
+        }
+        catch(error){
+            console.error(error);
+        }
+    } else {
+        console.log('now how did you manage that?')
+    }
+}
